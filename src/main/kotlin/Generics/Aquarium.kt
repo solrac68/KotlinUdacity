5a75ca852e69ce7368307cef7a1dab1be5176476
solrac68 <solrac.hincapie@gmail.com>
2021-10-26 12:32:31 -0500

    GEnerics prof

--- src/main/kotlin/Generics/Aquarium.kt
+++ src/main/kotlin/Generics/Aquarium.kt
@@ -1,5 +1,9 @@
 package Generics
 
+import Buildings.Building
+import Buildings.Wood
+import java.awt.AWTError
+
 open class WaterSupply(var needsProcessed: Boolean)
 
 class TapWater: WaterSupply(true){
@@ -16,9 +20,12 @@ class LakeWater: WaterSupply(true){
     }
 }
 
-class Aquarium<T:WaterSupply>(val waterSupply: T){
-    fun addWater() {
-        check(!waterSupply.needsProcessed) {"water supply needs processed"}
+class Aquarium<out T:WaterSupply>(val waterSupply: T){
+    fun addWater(cleaner: Cleaner<T>) {
+        //check(!waterSupply.needsProcessed) {"water supply needs processed"}
+        if(waterSupply.needsProcessed) {
+            cleaner.clean(waterSupply)
+        }
         println("adding water from $waterSupply")
     }
 }
@@ -36,5 +43,37 @@ fun genericExample(){
 
     val aquarium4 = Aquarium(LakeWater())
     aquarium4.waterSupply.filter()
-    aquarium4.addWater()
+    //aquarium4.addWater()
+}
+
+fun main(args: Array<String>) {
+    //val wood = Building<Wood>(Wood())
+    //wood.build()
+    genericExample3()
+}
+
+interface Cleaner<in T: WaterSupply>{
+    fun clean(waterSupply: T)
+}
+
+class TapWaterCleaner: Cleaner<TapWater>{
+    override fun clean(waterSupply: TapWater) {
+        waterSupply.addChemicalCleaners()
+    }
+}
+
+fun addItemTo(aquarium: Aquarium<WaterSupply>) = println("Item added")
+
+fun genericExample2(){
+    val aquarium = Aquarium(TapWater())
+
+    addItemTo(aquarium) // ES posible por que la clase Aquarium es marcada cómo out Aquarium<out T:WaterSupply>
+}
+
+fun genericExample3(){
+    val cleaner = TapWaterCleaner()
+
+    val aquarium = Aquarium(TapWater())
+    aquarium.addWater(cleaner)
+
 }
